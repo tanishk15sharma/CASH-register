20c0dafc11137ced2847a04c60ff8d3f3f6edab2
tanishk15sharma <tanishk1582002@gmail.com>
2021-09-21 12:45:40 +0530

    fix

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes")
 
-
 const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+
 
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     message.style.display = "none";
 
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
+    if ( Number(billAmount.value) > 0) {
+        if ( Number(cashGiven.value) >= Number (billAmount.value) ) {
+            const amountToBeReturned = Number(cashGiven.value) - Number(billAmount.value) ;
             calculateChange(amountToBeReturned);
 
         } else {
@@ -23,10 +23,7 @@ checkButton.addEventListener("click", function validateBillAndCashAmount() {
         showMessage("Invalid bill Amount");
 
     }
-
-
 });
-
 function calculateChange(amountToBeReturned) {
     for (let i=0; i< availableNotes.length; i++ ) {
         const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
@@ -34,8 +31,6 @@ function calculateChange(amountToBeReturned) {
         noOfNotes[i].innerText = numberOfNotes; 
 
     }
-
-
 }
 
 function showMessage(msg) {
